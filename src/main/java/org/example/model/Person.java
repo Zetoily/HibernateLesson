e1bb4bee6ee2328535668543d214f383085e48ca
Miron <miron.yusupov04@gmail.com>
2023-05-19 12:09:38 +0300

    Hibernate add/remove

---
+++ src/main/java/org/example/model/Person.java
@@ -0,0 +1,70 @@
+package org.example.model;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "Person")
+public class Person {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "age")
+    private int age;
+
+    public List<Item> getItems() {
+        return items;
+    }
+
+    public void setItems(List<Item> items) {
+        this.items = items;
+    }
+
+    @OneToMany(mappedBy = "owner")
+    private List<Item> items;
+
+    public Person(){}
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
